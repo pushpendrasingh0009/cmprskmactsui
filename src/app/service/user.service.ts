b8b8947b07f3ff4aee78b109de80cab25b071763
Nitesh Kumar <nitkumar13@publicisgroupe.net>
2019-07-31 14:51:55 +0530

    Bootstrap changes for conflict check page

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -13,17 +13,17 @@ export class UserService {
   tokenExpirationDate: Date;
   partyData: SimplePartyFuse[];
 
-  constructor(private http: HttpClient,private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   userAuthentication(username, password) {
-    let data = {'username':  username,  'password': password};
-    let reqHeader = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8', 'dataType':'json',  'No-Auth':'True'});
+    let data = { 'username': username, 'password': password };
+    let reqHeader = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8', 'dataType': 'json', 'No-Auth': 'True' });
     return this.http.post(API_URL + 'create/token', data, { headers: reqHeader });
   }
 
-  isUserLoggedIn(){
+  isUserLoggedIn() {
     let user = localStorage.getItem(USER_TOKEN);
-    return !(user===null);
+    return !(user === null);
   }
 
   logout() {
@@ -32,8 +32,8 @@ export class UserService {
   }
 
   getClientIpDetails(): Observable<Location> {
-    let reqHeader = new HttpHeaders({'Accept': 'application/json', 'No-Auth': 'True'});
-    return this.http.get<Location>(GET_LOCATION_URL,  { headers: reqHeader });
+    let reqHeader = new HttpHeaders({ 'Accept': 'application/json', 'No-Auth': 'True' });
+    return this.http.get<Location>(GET_LOCATION_URL, { headers: reqHeader });
   }
 
   getPartyData() {
@@ -42,7 +42,7 @@ export class UserService {
       data: {
         Isin: '562jsi2',
         SEDOL: 'teyyww',
-        CUSIP : 'N/A',
+        CUSIP: 'N/A',
         Ticker: 'YES',
         type: 'N/A'
       }
@@ -52,7 +52,7 @@ export class UserService {
       data: {
         Isin: '561234',
         SEDOL: 'YTER',
-        CUSIP : 'TER',
+        CUSIP: 'TER',
         Ticker: 'NO',
         type: 'N/A'
       }
@@ -62,7 +62,7 @@ export class UserService {
       data: {
         Isin: '561245',
         SEDOL: 'YTE1',
-        CUSIP : 'T45',
+        CUSIP: 'T45',
         Ticker: 'YES',
         type: 'MANUAL COMPANY'
       }
@@ -72,7 +72,7 @@ export class UserService {
       data: {
         Isin: '36445',
         SEDOL: 'EY1',
-        CUSIP : 'S43',
+        CUSIP: 'S43',
         Ticker: 'NO',
         type: 'N/A'
       }
@@ -82,7 +82,7 @@ export class UserService {
       data: {
         Isin: '38912',
         SEDOL: 'N/A',
-        CUSIP : '651',
+        CUSIP: '651',
         Ticker: 'N/A',
         type: 'AUTOMATED'
       }
@@ -92,7 +92,7 @@ export class UserService {
       data: {
         Isin: '98315',
         SEDOL: 'YUE',
-        CUSIP : 'S193',
+        CUSIP: 'S193',
         Ticker: 'YES',
         type: 'MANUAL'
       }
@@ -102,12 +102,73 @@ export class UserService {
       data: {
         Isin: '65123',
         SEDOL: 'GT1',
-        CUSIP : '8832',
+        CUSIP: '8832',
+        Ticker: 'YES',
+        type: 'AUTOMATED'
+      }
+    },
+    {
+      name: 'ZERT LTD',
+      data: {
+        Isin: '65123',
+        SEDOL: 'GE1',
+        CUSIP: '6532',
+        Ticker: 'NO',
+        type: 'Manual'
+      }
+    },
+    {
+      name: 'XTD LTD',
+      data: {
+        Isin: '65123',
+        SEDOL: 'GT1',
+        CUSIP: '8832',
+        Ticker: 'YES',
+        type: 'AUTOMATED'
+      }
+    },
+    {
+      name: 'LYR CO',
+      data: {
+        Isin: '11123',
+        SEDOL: 'TT6',
+        CUSIP: '7611',
+        Ticker: 'N/A',
+        type: 'N/A'
+      }
+    },
+    {
+      name: 'LMR Co',
+      data: {
+        Isin: '1223',
+        SEDOL: '87E1',
+        CUSIP: '0932',
+        Ticker: 'NO',
+        type: 'MANUAL'
+      }
+    },
+    {
+      name: 'UCRT COM',
+      data: {
+        Isin: '6754',
+        SEDOL: '981',
+        CUSIP: '9865',
+        Ticker: 'NA',
+        type: 'N/A'
+      }
+    },
+    {
+      name: 'LQW LTD',
+      data: {
+        Isin: '45123',
+        SEDOL: 'GQ1',
+        CUSIP: '8542',
         Ticker: 'YES',
         type: 'AUTOMATED'
       }
     }
-  ];
+
+    ];
     return this.partyData;
   }
 }
