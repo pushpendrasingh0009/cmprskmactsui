fae66510f7b27dca51190e5812e11de5ef434da7
Nitesh Kumar <nitkumar13@publicisgroupe.net>
2019-08-05 16:04:20 +0530

    changes for preconflict page design

--- src/app/pre-conflict-chk/pre-conflict-chk.component.ts
+++ src/app/pre-conflict-chk/pre-conflict-chk.component.ts
@@ -10,7 +10,7 @@ import { UserService } from '../service/user.service';
 })
 export class PreConflictChkComponent implements OnInit {
   searchData: SimplePartyFuse [] ;
-  output: SimplePartyFuse[];
+  output: SimplePartyFuse [] = [];
   parties: string;
   selectedParties: Set<string> ;
 
